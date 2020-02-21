c855cd0d28f00c2bf7f4d13bd36ae592a401baec
Ruby Quail <ruby@rubyquail.design>
2020-02-21 13:45:43 +1100

    update to fuseJS
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- fuse.js
+++ fuse.js
@@ -1,25 +1,24 @@
-const { FuseBox, WebIndexPlugin, RawPlugin } = require('fuse-box');
-const path = require('path');
-const fuse = FuseBox.init({
-  homeDir: 'src',
-  target: 'browser@esnest',
-  output: 'dist/$name.js',
-  sourceMaps: true,
-  plugins: [
-    WebIndexPlugin({
-      author: 'Ruby Quail',
-      title: path.basename(process.cwd()),
-      path: '.',
-      template: './src/index.html',
-    }),
-    RawPlugin(['*.frag', '*.vert']),
-  ],
+
+const { fusebox } = require('fuse-box'); // eslint-disable-line
+
+const fuse = fusebox({
+  entry: 'src/index.ts',
+  target: 'browser',
+  hmr: {
+    hardReloadScripts: true,
+  },
+  devServer: {
+    enabled: true,
+    httpServer: {
+      enabled: true,
+      port: 8080,
+    },
+  },
+  output: 'dist/app.js',
+  useSingleBundle: 'true',
+  webIndex: {
+    template: 'src/index.html',
+  },
+  dependencies:{include:['tslib']}
 });
-fuse.dev({
-  port: 8080,
-});
-fuse
-  .bundle('app')
-  .instructions(' > index.ts')
-  .watch();
-fuse.run();
+fuse.runDev();
