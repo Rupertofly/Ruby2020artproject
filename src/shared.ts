f60b448bfafdfe166e22295ed683804473b09a27
Ruby Quail <ruby@rubyquail.design>
2020-03-12 10:00:16 +1100

    start from scratch noe
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- src/shared.ts
+++
@@ -1,9 +0,0 @@
-export function drawCircle(
-    drawingContext: CanvasRenderingContext2D,
-    x: number,
-    y: number,
-    radius: number
-) {
-    drawingContext.beginPath();
-    drawingContext.ellipse(x, y, radius, radius, 0, 0, Math.PI * 2);
-}
